9f64f4b028e80f87082bb8ce53ffe071bbda9dd7
melon <ujwal5555@gmail.com>
2020-09-17 10:58:46 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import "./App.css";
+import SearchBar from "./components/searchBar/SearchBar";
+import { StylesProvider } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  items: {
+    margin: "10px",
+    borderRadius: "12px",
+    textAlign: "center",
+  },
+  rounded: {
+    borderRadius: "12px",
+  },
+});
 function App() {
+  const classes = useStyles();
+  const [name, setName] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="stretch"
+        spacing={2}
+      >
+        <Grid item xs={12} sm={10} lg={8} xl={8} className={classes.items}>
+          <StylesProvider injectFirst>
+            <SearchBar />
+          </StylesProvider>
+        </Grid>
+        <Grid item xs={6} sm={5} lg={4} xl={4} className={classes.items}>
+          <p>First</p>
+        </Grid>
+        <Grid item xs={6} sm={5} lg={4} xl={4} className={classes.items}>
+          <p>Second</p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar, { SearchIcon } from "material-ui-search-bar";
+
+function SearchBarJs() {
+  return (
+    <div>
+      <SearchBar
+        placeholder=""
+        style={{ background: "black", borderRadius: "15px" }}
+      />
+    </div>
+  );
+}
+
+export default SearchBarJs;
